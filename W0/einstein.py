56be1e79390ffc8b8ded78989714aa9d80fbf413
Cryss <128715006+TheRealCryss@users.noreply.github.com>
2023-05-11 22:08:22 +0200

    Add files via upload

---
+++ W0/einstein.py
@@ -0,0 +1,4 @@
+mass = int(input("Mass in kg to convert as joule (int)"))
+joule = mass * (300000000 ** 2)
+
+print(joule)
---
+++ W0/playback.py
@@ -0,0 +1,5 @@
+def main():
+    user_input = input("Tell me something: ")
+    print(user_input.replace(" ", "..."))
+
+main()
